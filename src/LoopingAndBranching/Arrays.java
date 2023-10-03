af9305d835375328a5ad647316f789f8c9333fc9
Chris <c.r.zambito@gmail.com>
2023-10-03 09:23:23 -0400

    push;

---
+++ src/LoopingAndBranching/Arrays.java
@@ -0,0 +1,24 @@
+package LoopingAndBranching;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+
+        numbers[0] = 4;
+        int count = 1;
+
+        for (int i = 0; i < numbers.length; i++, count++) {
+            numbers[i] = count;
+        }
+
+        System.out.println("-----");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
